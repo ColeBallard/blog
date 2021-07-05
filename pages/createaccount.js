ed2651286c3f2ffcf95cccee91fc8662ea4fde31
coleb <cole.ballard97@gmail.com>
2021-07-05 16:17:13 -0600

    added create account page in react

--- pages/createaccount.js
+++ pages/createaccount.js
@@ -1,9 +1,36 @@
-import { Container } from '@material-ui/core';
+import { Container, TextField, Button, Link } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    alignItems: 'center',
+    width: '50%',
+  },
+  textField: {
+    width: '100%',
+    marginBottom: '2ch'
+  },
+  linkText: {
+    fontSize: '1.5ch',
+    fontFamily: [
+      'Nunito Sans',
+      'sans-serif'
+    ],
+  }
+}));
 
 export default function CreateAccount() {
+  const classes = useStyles();
+
   return (
-    <Container>
-      Under construction
+    <Container className={classes.root}>
+      <TextField id="name" label="Name" variant="outlined" className={classes.textField} />
+      <TextField id="email" label="Email" variant="outlined" className={classes.textField} />
+      <TextField id="password" label="Password" type="password" variant="outlined" className={classes.textField} />
+      <Button>Create Account</Button>
+      <Link href='/login'>
+        <p className={classes.linkText}>Already have an account? Login.</p>
+      </Link>
     </Container>
   )
 }
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const next = require('next');
 var mysql = require('mysql');
 require('dotenv').config;
-var connection = mysql.createConnection(process.env.JAWSDB_URL);
+// var connection = mysql.createConnection(process.env.JAWSDB_URL);
     
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
@@ -10,26 +10,31 @@ const handle = app.getRequestHandler();
     
 app.prepare()
 .then(() => {
-  const server = express();
+  const exp = express();
 
-  connection.connect();
+  // connection.connect();
 
-  connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-    if (err) throw err;
+  // connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+  //   if (err) throw err;
 
-    console.log('The solution is: ', rows[0].solution);
+  //   console.log('The solution is: ', rows[0].solution);
+  // });
+
+  // connection.end();
+    
+  exp.get('*', (req, res) => {
+    return handle(req, res);
   });
 
-  connection.end();
-    
-  server.get('*', (req, res) => {
+  exp.get('/login',  (req, res)=> {
+    console.log('hey');
     return handle(req, res);
-  })
+  });
     
-  server.listen(3000, (err) => {
+  exp.listen(3000, (err) => {
     if (err) throw err;
     console.log('> Ready on http://localhost:3000');
-  })
+  });
 })
 .catch((ex) => {
   console.error(ex.stack);
