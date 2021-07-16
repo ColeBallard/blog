675cbddce52d8ed9bf5f3ff9a174dec1facdcb48
coleb <cole.ballard97@gmail.com>
2021-07-16 15:44:05 -0600

    wrap store around index

--- src/index.js
+++ src/index.js
@@ -2,4 +2,7 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import App from './App';
 
+import { Provider } from 'react-redux';
+import store from './app/store';
+
 ReactDom.render(<App />, document.getElementById('root'));
