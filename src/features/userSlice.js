5ee3112d0e391d940012e0d7923749c4a322287e
coleb <cole.ballard97@gmail.com>
2021-07-16 15:37:21 -0600

    added default export for userSlice obj

--- src/features/userSlice.js
+++ src/features/userSlice.js
@@ -30,3 +30,5 @@ export const selectSignedIn = state => state.user.isSignedIn;
 export const selectUserData = state => state.user.userData;
 export const selectSearchInput = state => state.user.searchInput;
 export const selectBlogData = state => state.user.blogData;
+
+export default userSlice.reducer;
