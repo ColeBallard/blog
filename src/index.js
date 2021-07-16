5ea934725895d2b105782f7e0250bd64b8902c13
coleb <cole.ballard97@gmail.com>
2021-07-16 15:48:28 -0600

    added provider to reactdom render

--- src/index.js
+++ src/index.js
@@ -5,4 +5,9 @@ import App from './App';
 import { Provider } from 'react-redux';
 import store from './app/store';
 
-ReactDom.render(<App />, document.getElementById('root'));
+ReactDom.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, 
+  document.getElementById('root')
+);
